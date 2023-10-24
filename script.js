ec592a7b69631555a3dc2794a52d561ae9f1cf28
Viviana Alba <vivianaalba7@gmail.com>
2023-10-23 22:06:12 -0700

    js code

---
+++ script.js
@@ -0,0 +1,16 @@
+let array1 = [2, 4, 6, 8, 10];
+
+console.log(`Array = ${array1}`)
+
+let sum;
+do {
+  sum = 0;
+  for (let i = 0; i < array1.length; i++) {
+    sum += array1[i];
+  }
+} while (array1.length[4]) 
+console.log(`Sum = ${sum}`)
+
+
+let average = sum / array1.length
+console.log(`Average = ${average}`)
